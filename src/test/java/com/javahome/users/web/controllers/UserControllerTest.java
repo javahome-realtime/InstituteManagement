77f9e6e392e0fc88ada72705a2d0abf7f46641d5
ramakanth.konday <ramakanth.konday@gmail.com>
2014-11-25 07:53:51 +0530

    Intial test setup

---
+++ src/test/java/com/javahome/users/web/controllers/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.javahome.users.web.controllers;
+
+import org.junit.Test;
+
+public class UserControllerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
